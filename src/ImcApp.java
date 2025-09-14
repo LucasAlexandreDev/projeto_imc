9604651d69ace31befafaedd189122a60470f1be
LucasAlexandreDev <lucasalexandre.lads13@gmail.com>
2025-09-14 20:23:34 -0300

    Update ImcApp.java

--- src/ImcApp.java
+++ src/ImcApp.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class ImcApp {
+    public static void main(String[] args) {
+        System.out.println("Iniciando o programa...");
+        Imc novo= new Imc();
+        novo.ReceberDadosDoPaciente();
+        System.out.println("Muito obrigado por usar o meu programa:)");
+    }
 }
